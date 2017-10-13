e6c49bb5441e3438ef20070b884a2811e8497816
meriadec <meriadec.pillet@gmail.com>
2017-10-13 13:51:02 +0200

    express

--- src/index.js
+++ src/index.js
@@ -1,46 +1,21 @@
 require('dotenv').config()
+
+const twilio = require('twilio')
 const http = require('http')
+const express = require('express')
 
 const { getStationsInfos } = require('./velib')
 
-const server = http.createServer(handler)
 const port = process.env.PORT
+const app = express()
 
-server.listen(port, err => {
-  if (err) {
-    console.log('x Problem with the server', err)
-    process.exit(1)
-  }
-  console.log(`server listening on port ${port}`)
+app.post('/message', function(req, res) {
+  const twiml = new twilio.TwimlResponse()
+  twiml.message('What?')
+  res.writeHead(200, { 'Content-Type': 'text/xml' })
+  res.end(twiml.toString())
 })
 
-async function handler(req, res) {
-  try {
-    const body = await parseBody(req)
-    console.log(body)
-    res.writeHead(200)
-    res.end('OK')
-  } catch (err) {
-    res.writeHead(400)
-    console.log(err)
-    res.end(err.message)
-  }
-}
-
-function parseBody(req) {
-  return new Promise((resolve, reject) => {
-    let data = ''
-    req.on('data', chunk => (data += chunk))
-    req.on('end', () => {
-      try {
-        const body = data
-          .split('&')
-          .map(pair => pair.split('=').map(t => decodeURIComponent(t)))
-          .reduce((acc, pair) => ({ ...acc, [pair[0]]: pair[1] }), {})
-        resolve(body)
-      } catch (err) {
-        reject(err)
-      }
-    })
-  })
-}
+http.createServer(app).listen(port, function() {
+  console.log(`server listening on port ${port}`)
+})
