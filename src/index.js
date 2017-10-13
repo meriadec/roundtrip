1db835a5b832d97370fc7e0db7f93791ecb46885
meriadec <meriadec.pillet@gmail.com>
2017-10-13 15:02:19 +0200

    parse body with express

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ require('dotenv').config()
 const Twilio = require('twilio')
 const http = require('http')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const { getStationsInfos } = require('./velib')
 
 const port = process.env.PORT
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
 const client = new Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
 
 app.post('/message', async function(req, res) {
   const msg = await client.messages.create({
     body: 'What?',
-    to: req.params.From,
+    to: req.body.From,
     from: process.env.TWILIO_NUMBER,
   })
   res.writeHead(200, { 'Content-Type': 'text/xml' })
